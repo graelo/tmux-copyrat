90132863d5b1da2e5feab801169c7be5ff02e87a
graelo <graelo@grael.cc>
2020-06-01 07:30:00 +0200

    feat|search for specific pattern names

--- src/regexes.rs
+++ src/regexes.rs
@@ -1,3 +1,5 @@
+use crate::error;
+
 pub const EXCLUDE_PATTERNS: [(&'static str, &'static str); 1] =
     [("ansi_colors", r"[[:cntrl:]]\[([0-9]{1,2};)?([0-9]{1,2})?m")];
 
@@ -5,15 +7,15 @@ pub const PATTERNS: [(&'static str, &'static str); 14] = [
     ("markdown_url", r"\[[^]]*\]\(([^)]+)\)"),
     (
         "url",
-        r"((https?://|git@|git://|ssh://|ftp://|file:///)[^ ]+)",
+        r"((https?://|git@|git://|ssh://|ftp://|file:///)[^ \(\)\[\]\{\}]+)",
     ),
     ("diff_a", r"--- a/([^ ]+)"),
     ("diff_b", r"\+\+\+ b/([^ ]+)"),
     ("docker", r"sha256:([0-9a-f]{64})"),
     ("path", r"(([.\w\-@~]+)?(/[.\w\-@]+)+)"),
-    ("color", r"#[0-9a-fA-F]{6}"),
+    ("hexcolor", r"#[0-9a-fA-F]{6}"),
     (
-        "uid",
+        "uuid",
         r"[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}",
     ),
     ("ipfs", r"Qm[0-9a-zA-Z]{44}"),
@@ -23,3 +25,15 @@ pub const PATTERNS: [(&'static str, &'static str); 14] = [
     ("address", r"0x[0-9a-fA-F]+"),
     ("number", r"[0-9]{4,}"),
 ];
+
+/// Type-safe string Pattern Name (newtype).
+#[derive(Debug)]
+pub struct NamedPattern(pub String, pub String);
+
+/// Parse a name string into `NamedPattern`, used during CLI parsing.
+pub fn parse_pattern_name(src: &str) -> Result<NamedPattern, error::ParseError> {
+    match PATTERNS.iter().find(|&(name, _pattern)| name == &src) {
+        Some((name, pattern)) => Ok(NamedPattern(name.to_string(), pattern.to_string())),
+        None => Err(error::ParseError::UnknownPatternName),
+    }
+}
