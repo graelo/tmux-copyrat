7550895602c9f1f7e63b0959085717932cee8c3e
graelo <graelo@grael.cc>
2021-03-23 22:20:21 +0100

    fix: default colors

--- src/ui/colors.rs
+++ src/ui/colors.rs
@@ -12,15 +12,15 @@ pub fn parse_color(src: &str) -> Result<Box<dyn color::Color>, error::ParseError
         "magenta" => Ok(Box::new(color::Magenta)),
         "cyan" => Ok(Box::new(color::Cyan)),
         "white" => Ok(Box::new(color::White)),
-        "bright-black" => Ok(Box::new(color::LightBlack)),
-        "bright-red" => Ok(Box::new(color::LightRed)),
-        "bright-green" => Ok(Box::new(color::LightGreen)),
-        "bright-yellow" => Ok(Box::new(color::LightYellow)),
-        "bright-blue" => Ok(Box::new(color::LightBlue)),
-        "bright-magenta" => Ok(Box::new(color::LightMagenta)),
-        "bright-cyan" => Ok(Box::new(color::LightCyan)),
-        "bright-white" => Ok(Box::new(color::LightWhite)),
-        // "default" => Ok(Box::new(color::Reset)),
+        "bright-black" | "brightblack" => Ok(Box::new(color::LightBlack)),
+        "bright-red" | "brightred" => Ok(Box::new(color::LightRed)),
+        "bright-green" | "brightgreen" => Ok(Box::new(color::LightGreen)),
+        "bright-yellow" | "brightyellow" => Ok(Box::new(color::LightYellow)),
+        "bright-blue" | "brightblue" => Ok(Box::new(color::LightBlue)),
+        "bright-magenta" | "brightmagenta" => Ok(Box::new(color::LightMagenta)),
+        "bright-cyan" | "brightcyan" => Ok(Box::new(color::LightCyan)),
+        "bright-white" | "brightwhite" => Ok(Box::new(color::LightWhite)),
+        "none" => Ok(Box::new(color::Reset)),
         _ => Err(error::ParseError::UnknownColor),
     }
 }
@@ -56,20 +56,20 @@ mod tests {
 #[clap(about)] // Needed to avoid this doc comment to be used as overall `about`.
 pub struct UiColors {
     /// Foreground color for base text.
-    #[clap(long, default_value = "bright-blue", parse(try_from_str = parse_color))]
+    #[clap(long, default_value = "bright-cyan", parse(try_from_str = parse_color))]
     pub text_fg: Box<dyn color::Color>,
 
     /// Background color for base text.
-    #[clap(long, default_value = "white", parse(try_from_str = parse_color))]
+    #[clap(long, default_value = "none", parse(try_from_str = parse_color))]
     pub text_bg: Box<dyn color::Color>,
 
     /// Foreground color for spans.
-    #[clap(long, default_value = "yellow",
+    #[clap(long, default_value = "blue",
                 parse(try_from_str = parse_color))]
     pub span_fg: Box<dyn color::Color>,
 
     /// Background color for spans.
-    #[clap(long, default_value = "white",
+    #[clap(long, default_value = "none",
                 parse(try_from_str = parse_color))]
     pub span_bg: Box<dyn color::Color>,
 
@@ -79,17 +79,17 @@ pub struct UiColors {
     pub focused_fg: Box<dyn color::Color>,
 
     /// Background color for the focused span.
-    #[clap(long, default_value = "white",
+    #[clap(long, default_value = "none",
                 parse(try_from_str = parse_color))]
     pub focused_bg: Box<dyn color::Color>,
 
     /// Foreground color for hints.
-    #[clap(long, default_value = "white",
+    #[clap(long, default_value = "yellow",
                 parse(try_from_str = parse_color))]
     pub hint_fg: Box<dyn color::Color>,
 
     /// Background color for hints.
-    #[clap(long, default_value = "magenta",
+    #[clap(long, default_value = "none",
                 parse(try_from_str = parse_color))]
     pub hint_bg: Box<dyn color::Color>,
 }
