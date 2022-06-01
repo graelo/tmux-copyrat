e13c58db30307b2f51c650305a66e6141c56373a
graelo <graelo@grael.cc>
2022-06-01 08:46:42 +0200

    refactor: better handling of hint_style_arg

--- src/lib.rs
+++ src/lib.rs
@@ -27,19 +27,6 @@ pub fn run(lines: &[&str], opt: &config::basic::Config) -> Option<ui::Selection>
         return None;
     }
 
-    let hint_style = match &opt.hint_style {
-        None => None,
-        Some(style) => match style {
-            config::basic::HintStyleArg::Bold => Some(ui::HintStyle::Bold),
-            config::basic::HintStyleArg::Italic => Some(ui::HintStyle::Italic),
-            config::basic::HintStyleArg::Underline => Some(ui::HintStyle::Underline),
-            config::basic::HintStyleArg::Surround => {
-                let (open, close) = opt.hint_surroundings;
-                Some(ui::HintStyle::Surround(open, close))
-            }
-        },
-    };
-
     let default_output_destination = config::extended::OutputDestination::Tmux;
 
     let selection: Option<ui::Selection> = {
@@ -49,7 +36,7 @@ pub fn run(lines: &[&str], opt: &config::basic::Config) -> Option<ui::Selection>
             default_output_destination,
             &opt.colors,
             &opt.hint_alignment,
-            hint_style,
+            opt.hint_style(),
         );
 
         ui.present()
