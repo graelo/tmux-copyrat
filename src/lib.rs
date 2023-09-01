6000913a53eb318483623d91490e494487155fbb
graelo <graelo@grael.cc>
2023-09-02 01:03:51 +0200

    chore: bump MSRV to 1.70

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@
 //! same functionality. For this Rust implementation, I got inspired by
 //! [tmux-thumbs], and I even borrowed some parts of his regex tests.
 //!
-//! Version requirement: _rustc 1.64+_
+//! Version requirement: _rustc 1.70+_
 //!
 //! ## Demo
 //!
